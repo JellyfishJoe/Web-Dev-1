46fb1a41c4bbcdd563449a9b736a301a56424a3f
JellyfishJoe <jellyfishjoe5@gmail.com>
2018-09-25 15:17:32 -0700

    fuvkdlsa;

--- js/quiz.js
+++ js/quiz.js
@@ -1,5 +1,3 @@
-alert("Welcome on the Internet");
-
 let quiz = {
 	"name": "Superhero Quiz",
 	"description": "How many heroes can you name?",
@@ -22,9 +20,12 @@ let quiz = {
 };		
 
 //These are the hooks that connect to our DOM obejcts
-var $question = document.getElementById('question');
+var $questionSpot = document.getElementById('questionSpot');
 var $feedback = document.getElementById('feedback');
 var $score = document.getElementById('score');
+
+let $form = document.forms.checkAnswer;
+let $input = $form.elements.answer;
 
 let $start = document.getElementById('start');
 //Common practice is to name vars with $ that matches the DOM obejct
@@ -44,22 +45,65 @@ function update(el, content, klass){
 	}
 }
 
+//Hide elements
+function hide(el) {
+	el.style.display = "none";
+}
+
+//Show elements
+function show(el) {
+	el.style.display = "block";
+}
+
+hide($form);
+hide($questionSpot);
+hide($feedback);
+hide($score);
+
 $start.addEventListener('click', function(){play(quiz)});
 
 //Create a function for playing the game
 function play(quiz) {
+	show($form);
+	show($questionSpot);
+	show($feedback);
+	show($score);
+
+	hide($start);
+
 	var score = 0;
+
+	var i = 0;
 	//This line will update the score while playing
 	update($score, score);
+
+	/*
 	for(var i = 0, question, answer; i < quiz.heroNames.length; i++) {
 		question = quiz.heroNames[i];
 		answer = ask(question);
 		check(answer);
 	}
-	
+	*/
+
+	function chooseQuestion() {
+		question = quiz.heroNames[i];
+		console.log(quiz.heroNames[i]);
+		answer = ask(question);
+	}
+
+	chooseQuestion();
+
+	$form.addEventListener("submit", function(event){
+		event.preventDefault();
+		console.log($input.value)
+		check($input.value);
+	});
+
 	function ask(question) {
-		update($question, quiz.question + question);	//This will show the question on the page
-		return prompt("What is the real name of " + quiz.heroNames[i] + "?"); // We still need the prompt to enter answers util we start using forms
+		update($questionSpot, quiz.question + question + "?");	//This will show the question on the page
+		//return prompt("What is the real name of " + quiz.heroNames[i] + "?"); // We still need the prompt to enter answers util we start using forms
+		$input.value = "";
+		$input.focus();
 	}
 
 	function check(answer) {
@@ -70,11 +114,15 @@ function play(quiz) {
 		} else {
 			update($feedback, "Wrong!", "wrong");
 		}
+		i++;
+		if(i === quiz.heroNames.length) {
+
+		} else {
+			chooseQuestion();
+		}
 	}
 
 	function gameOver() {
 		update($question, "Game over, you scored " + score + " points!");
 	}
-
-	gameOver();
 }
